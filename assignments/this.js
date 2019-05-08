86ba2301bdb26f38ec4618c971c2816ffd7573ba
Karim B <mattiakarim@live.it>
2019-05-08 13:11:38 +0200

    GIven answer to the first 4 questions of the this.js file regarding the this keyword bindings

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. New bindind -> This happens when creating a new object using the new keyword which creates an empty object bind to the this keyword in the function constructor and at the end of the same function constructor returns the this keyword, when the new object function gets invoked using the dot notation what is at the left of the dot represents what this reference to.
+* 2. Window binding -> This happens when the function is invoked without any object being present, in this case the binding of the this keyword refers to the window object. 
+* 3. Implicit binding -> This happens when the this keyword is used inside the method of an object, which in this case the this keyword points to the object itself in which the method is present.
+* 4. Explicit binding -> This happens when the this keyword is binded to a function using one of the 3 function methods which are .call() .apply() .bind() in which their first parameter will represent what the this keyword will reference to.
 *
 * write out a code example of each explanation above
 */
